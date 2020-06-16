12122349ec31e34cb4fd6d150a7a8eb934164035
Igor Jean Luc Ndiramiye <nigorjeanluc@gmail.com>
2020-06-16 10:23:43 +0200

    Fix session bug

--- config/app.php
+++ config/app.php
@@ -163,8 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-        Mews\Purifier\PurifierServiceProvider::class,
+        // Collective\Html\HtmlServiceProvider::class,
+        // Mews\Purifier\PurifierServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -178,7 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -229,9 +229,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
-        'Purifier' => Mews\Purifier\Facades\Purifier::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        // 'Html' => Collective\Html\HtmlFacade::class,
+        // 'Purifier' => Mews\Purifier\Facades\Purifier::class,
+        // 'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
